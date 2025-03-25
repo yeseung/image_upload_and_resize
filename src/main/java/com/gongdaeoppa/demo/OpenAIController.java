8b6ac64134743228808e97c4758737302f5ea815
yeseung <hayeseung@gmail.com>
2025-03-25 12:10:15 +0900

    ✨챗봇ai

--- src/main/java/com/gongdaeoppa/demo/OpenAIController.java
+++ src/main/java/com/gongdaeoppa/demo/OpenAIController.java
@@ -5,9 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
